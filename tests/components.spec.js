710c9009956f507c730552a8a95609b83aab4776
Elijah Meshnick <emeshnick@gmail.com>
2022-02-03 20:43:45 -0700

    set up enzyme

--- tests/components.spec.js
+++ tests/components.spec.js
@@ -2,19 +2,19 @@ import { expect } from "chai";
 import React from "react";
 import enzyme, { shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import { Home } from "../client/components/Home";
+import Home from "../client/components/Home";
 
 const adapter = new Adapter();
 enzyme.configure({ adapter });
 
 describe("Components", () => {
-  let userHome;
+  let home;
 
   beforeEach(() => {
     home = shallow(<Home />);
   });
 
   it("renders Home component", () => {
-    expect(home.find("1").text()).to.be.equal("Home");
+    expect(home.find("h1").text()).to.be.equal("Home");
   });
 });
