6ae602167b622fcebcbcfd1f18c43f086b713b05
Elijah Meshnick <emeshnick@gmail.com>
2022-02-05 18:54:52 -0700

    Set up for TMDB api

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { TMDBKey } from "../../secrets";
+import axios from "axios";
 
 const styles = {
   mainContainer: {
     padding: "5rem",
   },
 };
+
 class Home extends React.Component {
+  async request() {
+    const res = axios.get(
+      `https://api.themoviedb.org/3/movie/76341?api_key=${TMDBKey}`
+    );
+    console.log(res);
+  }
+
   render() {
+    this.request();
     return (
       <Container style={styles.mainContainer}>
         <h1>Home</h1>
