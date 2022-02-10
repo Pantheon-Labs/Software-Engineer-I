5d1c1dde5e2b3a95cdac04ca431055500f87d446
Elijah Meshnick <emeshnick@gmail.com>
2022-02-10 12:58:04 -0700

    Added comments

--- client/store/index.js
+++ client/store/index.js
@@ -4,10 +4,14 @@ import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import data from "./data.js";
 
+/*
+ * Reducer composed with middleware
+ */
 const reducer = combineReducers({ data });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
 const store = createStore(reducer, middleware);
 
+// Export store for use in React components
 export default store;
