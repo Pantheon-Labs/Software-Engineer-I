a9ddaaad0f8c5deb36d8ca04b33137859fe15b65
Elijah Meshnick <emeshnick@gmail.com>
2022-02-10 13:00:11 -0700

    Added comments

--- client/store/singleStar.js
+++ client/store/singleStar.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 import { TMDBKey } from "../../secrets";
 
+/*
+ * Action creators, thunks, and reducer for individual celebrities
+ */
+
 const GET_STAR = "GET_STAR";
 
+// Action creator for dispatch
 const gotStar = (star) => {
   return {
     type: GET_STAR,
@@ -10,8 +15,10 @@ const gotStar = (star) => {
   };
 };
 
+// Call to api and then dispatch
 export const getStar = (name) => {};
 
+// Reducer contains all information from star API call
 export default function (state = {}, action) {
   switch (action.type) {
     case GET_STAR:
