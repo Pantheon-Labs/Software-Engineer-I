b865a0ec6c940fd27f4980be52c5dd5cf798d68d
Connor <connor.marble@gmail.com>
2022-02-15 14:13:03 -0500

    added data to server

--- server/src/controllers/skills/getSkills/getSkills.ts
+++ server/src/controllers/skills/getSkills/getSkills.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response, NextFunction } from "express";
+import { data } from "../../../db/data";
 
 const getSkills = (req: Request, res: Response, next: NextFunction): void => {
-  res.send({ status: "ok" });
+  res.send({ status: "ok", data: JSON.stringify(data) });
 };
 
 export default getSkills;
---
+++ server/src/db/data.ts
@@ -0,0 +1,36 @@
+export const data = {
+  skills: [
+    {
+      title: "Javascript",
+      years: "5"
+    },
+    {
+      title: "React",
+      years: "4"
+    },
+    {
+      title: "React Native",
+      years: "3"
+    },
+    {
+      title: "Typescript",
+      years: "3"
+    },
+    {
+      title: "Node",
+      years: "4"
+    },
+    {
+      title: "GraphQL",
+      years: "3"
+    },
+    {
+      title: "PostgreSQL",
+      years: "3"
+    },
+    {
+      title: "MongoDB",
+      years: "4"
+    }
+  ]
+};
