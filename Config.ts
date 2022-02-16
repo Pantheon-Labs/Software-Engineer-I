6226583c3b03ed09933d599215139dfd042b8680
Jose Valerio <contact@josevalerio.com>
2022-02-16 08:17:42 -0500

    Cleaned up cdk file formatting

--- Config.ts
+++ Config.ts
@@ -4,11 +4,85 @@ export const NANOID_ALPHABET = "1234567890abcdefghijklmnopqrstuvwxyz";
 export const ALLOWED_FILE_TYPES = [".jpeg", ".png", ".jpg"];
 
 // How many labels should Rekognition return
-export const MAX_LABELS = 3;
+export const MAX_LABELS = 5;
 
+// https://docs.aws.amazon.com/polly/latest/dg/SupportedLanguage.html
 export enum LANGUAGE_CODES {
   SPANISH = "es-MX",
   RUSSIAN = "ru-RU",
   JAPANESE = "ja-JP",
   FRENCH = "fr-FR",
 }
+
+/**
+ * NOTE: Must be supported by Polly!
+ * https://docs.aws.amazon.com/polly/latest/dg/SupportedLanguage.html
+ */
+export const TRANSLATION_SETTINGS = {
+  service: "translate",
+  action: "translateText",
+  iamResources: ["*"],
+  inputPath: "$.Name",
+  parameters: {
+    SourceLanguageCode: "en",
+    TargetLanguageCode: "en",
+    "Text.$": "$",
+  },
+  resultPath: "$.translationResults",
+};
+
+export const WAF_SETTINGS = {
+  name: `${process.env.NODE_ENV}-API-WAF`,
+  description: "Blocks IPs that make too many requests",
+  defaultAction: {
+    allow: {},
+  },
+  scope: "CLOUDFRONT",
+  visibilityConfig: {
+    cloudWatchMetricsEnabled: true,
+    metricName: "cloudfront-ipset-waf",
+    sampledRequestsEnabled: true,
+  },
+  rules: [
+    {
+      name: `too-many-requests-rule`,
+      priority: 0,
+      statement: {
+        rateBasedStatement: {
+          limit: 300, // In a 5 minute period
+          aggregateKeyType: "IP",
+        },
+      },
+      action: {
+        block: {
+          customResponse: {
+            responseCode: 429,
+          },
+        },
+      },
+      visibilityConfig: {
+        sampledRequestsEnabled: true,
+        cloudWatchMetricsEnabled: true,
+        metricName: `${process.env.NODE_ENV}-WAF-BLOCKED-IPs`,
+      },
+    },
+    {
+      name: "AWS-AWSManagedRulesAmazonIpReputationList",
+      priority: 2,
+      statement: {
+        managedRuleGroupStatement: {
+          vendorName: "AWS",
+          name: "AWSManagedRulesAmazonIpReputationList",
+        },
+      },
+      overrideAction: {
+        none: {},
+      },
+      visibilityConfig: {
+        sampledRequestsEnabled: false,
+        cloudWatchMetricsEnabled: true,
+        metricName: "AWS-AWSManagedRulesAmazonIpReputationList",
+      },
+    },
+  ],
+};
