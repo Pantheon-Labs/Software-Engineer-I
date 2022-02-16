1de1252d31adf65f0e9da2491b918ffc98d246b6
Jose Valerio <contact@josevalerio.com>
2022-02-16 04:49:09 -0500

    Creaetd polly SDK call

--- functions/audio-processor.ts
+++ functions/audio-processor.ts
@@ -1,10 +1,41 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import {
+  Polly,
+  StartSpeechSynthesisTaskCommand,
+  StartSpeechSynthesisTaskCommandInput,
+  SynthesizeSpeechCommand,
+} from "@aws-sdk/client-polly";
+import { pollyClient } from "../awsClients/pollyClient";
 
-export const main = async (
-  event: APIGatewayProxyEventV2
-): Promise<APIGatewayProxyResultV2> => {
-  console.log(event);
+// Input format:
+// {
+//   Confidence: 85.85332,
+//   Instances: [],
+//   Name: 'Man',
+//   Parents: [ { Name: 'Person' } ],
+//   translationResults: {
+//     SourceLanguageCode: 'en',
+//     TargetLanguageCode: 'ja',
+//     TranslatedText: 'おとこ'
+//   }
+// }
+// TODO types
+export const main = async (event: any) => {
   console.log(JSON.stringify(event));
+
+  const S3Key = `/audio/${event.name}/${event.translationResults.TargetLanguageCode}`;
+  const params: StartSpeechSynthesisTaskCommandInput = {
+    OutputFormat: "mp3",
+    OutputS3BucketName: process.env.BUCKET_NAME,
+    OutputS3KeyPrefix: S3Key,
+    Text: event.translationResults.TranslatedText,
+    VoiceId: "Joanna",
+  };
+
+  // TODO - Check if file exists
+  try {
+    await pollyClient.send(new SynthesizeSpeechCommand(params));
+  } catch (error) {}
   return {
     statusCode: 200,
     body: JSON.stringify({
