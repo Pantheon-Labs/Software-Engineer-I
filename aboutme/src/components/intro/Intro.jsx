b00f467e00fcc9b61b763349ec9384a865240a4e
kenliii <90875261+kenliii@users.noreply.github.com>
2022-02-18 02:33:32 -0800

    finished intro

--- aboutme/src/components/intro/Intro.jsx
+++ aboutme/src/components/intro/Intro.jsx
@@ -8,7 +8,20 @@ export default function intro() {
             <img src="images/IMG_1052.jpeg" alt=""/>
           </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+            <div className="wrapper">
+              <h1> Hi! </h1>
+              <p> Welcome to my page,</p>
+              <p> my names Ken, I am currently a student studying <br></br> software engineering with General Assembly. <br></br>I am currently 21 years old, I live in Seattle WA </p>
+              <br></br>
+              <br></br>
+              <h1>Hobbies!</h1>
+              <p> I go to music festivals, hikes, <br></br> and i especially love sleeping.</p>
+            </div>
+                  <a href="portfolio">
+                    <img src="a" alt="" />
+                  </a>
+        </div>
     </div>
   )
 }
