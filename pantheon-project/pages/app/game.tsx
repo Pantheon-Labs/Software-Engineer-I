048d308fa241fb55ade6d53d362f7fd411de7a2c
Nick Vitale <ntxnick@aol.com>
2022-02-19 12:29:38 -0600

    updated game format

--- pantheon-project/pages/app/game.tsx
+++ pantheon-project/pages/app/game.tsx
@@ -1,4 +1,4 @@
-import { VStack, Heading, Text, Image, HStack, Button, Box } from '@chakra-ui/react';
+import { VStack, Heading, Text, Image, Button, Box, SimpleGrid, Center } from '@chakra-ui/react';
 import { NextPage } from 'next'
 import React, {useState, useEffect} from 'react';
 
@@ -57,58 +57,62 @@ const Game: NextPage = () => {
 
   return (
 
-    <VStack alignItems="center">
-        <Heading size='3xl' pt="5" pb='5'>
-            Rock Paper Scissors
-        </Heading>
+    <VStack alignItems="center">]
+        <SimpleGrid columns={1} spacing = {10}>
+            <Heading size='3xl' pt="5" pb='5'>
+                Rock Paper Scissors
+            </Heading>
+        </SimpleGrid>
 
-        <HStack pt='2' spacing='45'>
-            <Box w='220px' h='50px'>
-                <Heading size="lg">
+        <SimpleGrid columns={2} spacing = {10} spacingY='15px'>
+
+            <Box height={'45'}>
+                <Heading textAlign={'center'} size="lg">
                     User Points
                 </Heading>
             </Box>
-            <Box w='250px' h='50px'>
-                <Heading size="lg">
+            <Box height={'45'}>
+                <Heading textAlign={'center'} size="lg">
                     Computer Points
                 </Heading>
             </Box>
-        </HStack>
 
-        <HStack>
-            <Box w='290px' h='50px'>
-                <Heading pr='11'>
+            <Box height={'45'}>
+                <Heading textAlign={'center'}>
                     {userPoints}
                 </Heading>
             </Box>
-            <Box w='100px' h='50px'>
-                <Heading>
+            <Box height={'45'}>
+                <Heading textAlign={'center'}>
                     {machinePoints}
                 </Heading>
             </Box>
-        </HStack>
+        </SimpleGrid>
 
-        <Heading pb='5'>
-            <Box w='300px'>
-                Win rate: {playerAverage}%
-            </Box>
-        </Heading>
+        <SimpleGrid columns={1} spacing = {10}>
+            <Heading pb='5'>
+                <Box w='300px'>
+                    Win rate: {playerAverage}%
+                </Box>
+            </Heading>
+        </SimpleGrid>
 
-        <HStack>
-            <Image boxSize='200px' src={`/${userChoice}.png`} alt=''></Image>
-            <Image boxSize='200px' src={`/${computerChoice}.png`} alt=''></Image>
-        </HStack>
+        <SimpleGrid columns={2} spacing = {20}>
+                <Image boxSize='200px' src={`/${userChoice}.png`} alt=''></Image>
+                <Image boxSize='200px' src={`/${computerChoice}.png`} alt=''></Image>
+        </SimpleGrid>
 
-        <HStack>
-            <Button onClick={() => handleOnClick('rock')}>Rock</Button>
-            <Button colorScheme={'green'} onClick={() => handleOnClick('paper')}>Paper</Button>
-            <Button onClick={() => handleOnClick('scissors')}>Scissors</Button>
-        </HStack>
+        <SimpleGrid columns={3} spacing = {20}>
+            <Button size='lg' colorScheme={'orange'} onClick={() => handleOnClick('rock')}>Rock</Button>
+            <Button size='lg' colorScheme={'green'} onClick={() => handleOnClick('paper')}>Paper</Button>
+            <Button size='lg' colorScheme={'teal'} onClick={() => handleOnClick('scissors')}>Scissors</Button>
+        </SimpleGrid>
 
 
         <Text>Turn Results: {turnResult}</Text>
 
-        <Button onClick={()=> reset()}>Reset the game</Button>
+        <Button colorScheme='red' onClick={()=> reset()}>Reset the game</Button>
+        <Button as="a" colorScheme="blue" href="/app/nextPage">Next Page</Button>
 
       </VStack>
   )
