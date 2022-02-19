4457cb22fdf9d53b0bbb8bcbe550bb7fc8cdd191
Nick Vitale <ntxnick@aol.com>
2022-02-19 17:09:33 -0600

    smooth page transition and small edits

--- pantheon-project/pages/_app.tsx
+++ pantheon-project/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import {ChakraProvider, extendTheme} from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 
 const config = {
   useSystemColorMode: false,
@@ -8,11 +9,20 @@ const config = {
 
 const theme = extendTheme({ config });
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
-  <ChakraProvider theme={theme}>
-    <Component {...pageProps} />
-  </ChakraProvider>
+    <motion.div key={router.route} initial='pageInitial' animate='pageAnimate' variants={{
+      pageInitial: {
+        opacity: 0
+      },
+      pageAnimate: {
+        opacity: 1
+      },
+    }}>
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  </motion.div>
   )
 }
 
--- pantheon-project/pages/index.tsx
+++ pantheon-project/pages/index.tsx
@@ -5,14 +5,15 @@ const Home: NextPage = () => {
   return (
 
     <VStack alignItems="center">
-      <Heading size="4xl">
+      <Heading size="4xl" pt='23'>
         Welcome to My App! 
       </Heading>
-      <Text fontSize="4xl">
-        This is the homepage.
+      <Text fontSize="3xl" w='80%' pt='50' textAlign={'center'}>
+        I created a little game to help refresh my React skills, and get better with formatting using Chakra-ui!
       </Text>
+      <Text fontSize="2xl">In order to move onto the next page, please click the button below!</Text>
       <Button as="a" colorScheme="blue" href="/app/game">
-        Next Page!
+        Go to Game
       </Button>
     </VStack>
   )
