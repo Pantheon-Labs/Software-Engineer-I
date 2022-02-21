7e371849ce7cf115430e75f250edd7d4faf177ca
Abraham <abdiaz2018@gmail.com>
2022-02-20 21:10:52 -0800

    creating strengths component

---
+++ hire-me-app/src/Strengths.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+
+class Strengths extends Component {
+    render() {
+        return (
+            <div>
+                <h3>My Strengths</h3>
+                <p></p>
+            </div>
+        )
+    }
+}
+
+export default Strengths;
