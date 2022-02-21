79528ecf21507d0d67e1a700a7d289748dcd9724
Abraham <abdiaz2018@gmail.com>
2022-02-20 23:23:06 -0800

    adding Link

--- hire-me-app/src/Navbar.js
+++ hire-me-app/src/Navbar.js
@@ -9,7 +9,8 @@ class Navbar extends Component {
                 <Link className='link' to="/">About Me</Link> |   
                 <Link className='link' to="/strengths">My strengths</Link> |   
                 <Link className='link' to="/passions">My passions</Link> |  
-                <Link className='link' to="/whyme">Why I want to join you</Link>
+                <Link className='link' to="/why">Why I want to join you</Link> |
+                <Link className='link' to="/pina-colada">Piña Colada</Link>
             </div>
         )
     }
