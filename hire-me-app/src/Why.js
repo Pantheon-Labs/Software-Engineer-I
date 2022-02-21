5ee178b9152150cbf30d239f06c980da96407616
Abraham <abdiaz2018@gmail.com>
2022-02-20 21:24:01 -0800

    creating why element

---
+++ hire-me-app/src/Why.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+
+class Why extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Why I want to join you</h3>
+                <p></p>
+            </div>
+        )
+    }
+}
+export default Why;
