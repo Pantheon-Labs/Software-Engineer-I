158c6b3549657df1f5796069f41194d0fc46224a
Abraham <abdiaz2018@gmail.com>
2022-02-20 23:34:40 -0800

    finishing component

--- hire-me-app/src/Why.js
+++ hire-me-app/src/Why.js
@@ -6,7 +6,7 @@ class Why extends Component {
         return (
             <div>
                 <h3>Why I want to join you</h3>
-                <p></p>
+                <p>I would like to work for your company because I really feel like your company can take off and be successful. When I visited the company site it really caught my eye and I would like to help this company grow.</p>
             </div>
         )
     }
