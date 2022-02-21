c563d1a521451e1841eb6d8e99df3ededd33cbe4
Abraham <abdiaz2018@gmail.com>
2022-02-20 21:09:18 -0800

    creating passions component

---
+++ hire-me-app/src/Passions.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+
+class Passions extends Component {
+    render() {
+        return (
+            <div>
+                <h3>My passions</h3>
+                <p></p>
+            </div>
+        )
+    }
+}
+export default Passions;
