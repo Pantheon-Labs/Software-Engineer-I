9d2626c337399ef88c0fae7d38f607d86a8acc2a
Abraham <abdiaz2018@gmail.com>
2022-02-20 16:58:43 -0800

    adding links

--- hire-me-app/src/App.js
+++ hire-me-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './Home';
+import { Link } from "react-router-dom"
 
 function App() {
   return (
@@ -8,6 +9,10 @@ function App() {
         <h1>
           Hi I'm Abraham
         </h1>
+        <Link className='link' to="/aboutme">About Me</Link>
+        <Link className='link' to="/strengths">My strengths</Link>
+        <Link className='link' to="/passions">My passions</Link>
+        <Link className='link' to="/whyme">Why I want to Join you</Link>
       </header>
       <Home/>
     </div>
