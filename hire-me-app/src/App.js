67e016b074fa1c231511793bf3dd6abbe876f85b
Abraham <abdiaz2018@gmail.com>
2022-02-20 17:06:30 -0800

    moving links into Navbar

--- hire-me-app/src/App.js
+++ hire-me-app/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Home from './Home';
-import { Link } from "react-router-dom"
 
 function App() {
   return (
@@ -9,10 +8,6 @@ function App() {
         <h1>
           Hi I'm Abraham
         </h1>
-        <Link className='link' to="/aboutme">About Me</Link>
-        <Link className='link' to="/strengths">My strengths</Link>
-        <Link className='link' to="/passions">My passions</Link>
-        <Link className='link' to="/whyme">Why I want to Join you</Link>
       </header>
       <Home/>
     </div>
--- hire-me-app/src/Navbar.js
+++ hire-me-app/src/Navbar.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom"
+
 
 class Navbar extends Component {
     render() {
         return(
             <div>
-
+                <Link className='link' to="/aboutme">About Me</Link>
+                <Link className='link' to="/strengths">My strengths</Link>
+                <Link className='link' to="/passions">My passions</Link>
+                <Link className='link' to="/whyme">Why I want to Join you</Link>
             </div>
         )
     }
