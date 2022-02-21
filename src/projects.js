ef075aeeae0b23a0642085cba130339172709f79
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-02-21 13:05:14 -0800

    project photo

--- src/projects.js
+++ src/projects.js
@@ -4,23 +4,7 @@ export const projects = [
                     subtitle: "MERN Stack",
                     description:
                               "A single page React app designed to help anyone choose what drink to order a bar",
-                    image: "./project-1.gif",
+                    image: "./public/bas-best-old-fashioned.jpg",
                     link: "https://what-to-drink-1.herokuapp.com/",
-          },
-          {
-                    title: "Disqord",
-                    subtitle: "Java, MySQL",
-                    description:
-                              "A fully functional chat application using Websockets",
-                    image: "./project-2.gif",
-                    link: "https://google.com",
-          },
-          {
-                    title: "Floral Arts Marketplace",
-                    subtitle: "Python, Flask, MySQL",
-                    description:
-                              "A fully functional e-marketplace to purchase flowers for any occasion",
-                    image: "./project-3.gif",
-                    link: "https://floral-arts-marketplace.herokuapp.com/",
           }
 ];
