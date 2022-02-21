b707feb2768dd6029f7a2cf897a57836ec61c6a0
Abraham <abdiaz2018@gmail.com>
2022-02-20 23:25:22 -0800

    creating pina colada component

---
+++ hire-me-app/src/PinaColada.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+
+class PinaColada extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default PinaColada;
