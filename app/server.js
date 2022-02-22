527cb5221ca2bc8ad2086dde811d0f7cb152cbb0
Daniel Martinez-Gamboa <dmartinezgamboa@gmail.com>
2022-02-22 14:53:41 -0800

    Add html & script.js

--- app/server.js
+++ app/server.js
@@ -5,21 +5,20 @@ const returnAPIKey = require("./key");
 const path = require("path");
 
 async function run() {
+  // spotify bearer token
   var client_id = process.env.CLIENT_ID;
   var client_secret = process.env.CLIENT_SECRET;
   var bearer = await returnAPIKey(client_id, client_secret);
   console.log(bearer);
 
+  // server
   const PORT = 8888;
-
   const app = express();
 
   app.use(express.static(path.join(__dirname, "public")));
-
-  // possibly not needed
-  // app.get("/", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "./public/html/index.html"));
-  // });
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/html/index.html"));
+  });
 
   app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}/`);
